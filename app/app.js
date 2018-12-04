bb700b500c94cd96398d3c8b7bb6ffce9dbcf40e
Ogie Ben <bensoft2k5@gmail.com>
2018-12-04 22:09:23 +0100

    removed redundant routes

--- app/app.js
+++ app/app.js
@@ -1,10 +1,29 @@
 import express from 'express';
+import apiRouter from './api/api';
+import createError from 'http-errors';
+import path from 'path';
+
 
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send("Welcome to my Test");
-})
+app.use('/api/v1/', apiRouter);
+
+app.use(function (req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function (err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+
 
 export default app;
