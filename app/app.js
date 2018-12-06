d6ec96da0c5dd43da5a1bce06ecd561395833bd8
Ogie Ben <bensoft2k5@gmail.com>
2018-12-07 00:53:28 +0100

    added jwt support

--- app/app.js
+++ app/app.js
@@ -1,16 +1,50 @@
 import express from 'express';
 import apiRouter from './api/api';
 import createError from 'http-errors';
-import path from 'path';
+import expressJwt from 'express-jwt';
+import axios from 'axios';
+import bodyparser from 'body-parser';
 
 
 
 const app = express();
 
+const jwtCheck = expressJwt({
+    secret: process.env.APP_AUTH_SECRET,
+    audience: 'https://localhost',
+    issuer: "https://slackdemo.auth0.com/"
+  });
+
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+app.use('/login', (req, res, next) => {
+    axios
+        .post('https://slackdemo.auth0.com/oauth/token', {
+            "client_id":process.env.APP_CLIENT_ID,
+            "client_secret": process.env.APP_CLIENT_SECRET,
+            "audience":"https://localhost",
+            "grant_type":"client_credentials"
+        })
+        .then((response) => {
+            
+            const access_token = response.data.access_token
+            res.json({
+                access_token
+            });
+        })
+        .catch((err) => {
+            res.sendStatus(500);
+        });
+    
+    
+});
+
+app.use(jwtCheck);
 app.use('/api/v1/', apiRouter);
 
 app.use(function (req, res, next) {
-    next(createError(404));
+    next(createError(404, "API route not found!! please check api route again."));
   });
   
   // error handler
