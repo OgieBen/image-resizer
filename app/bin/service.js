af7b5bd0d8290a349e4056024977041b9a4c7435
Ogie Ben <bensoft2k5@gmail.com>
2018-12-07 23:51:47 +0100

    added proper docs to file

--- app/bin/service.js
+++ app/bin/service.js
@@ -11,12 +11,23 @@ server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening)
 
+
+/**
+ * Error handler for our server
+ * @param {Error} error 
+ * @throws {Error}
+ */
 function onError(error){
     if(error.syscall != 'listen'){
         throw error;
     }
 }
 
+
+/**
+ * A simple function that displays feedback to user
+ * 
+ */
 function onListening(){
     console.log("Server is listeneing on port: " + server.address().port);
 }
